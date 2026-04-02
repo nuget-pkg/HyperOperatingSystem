a297a46966e27c3860ad7fca9eb3dfa81482ace6
javacommons <javacommons@gmail.com>
2026-04-02 18:07:07 +0900

    ⭕️ [️COMMIT] for tag: v2026.0402.1806.57 [DIFF] [/DIFF] [ADDED] HyperOperatingSystem.Demo/.gitconfig [ADDED] HyperOperatingSystem.Demo/HyperOperatingSystem.Demo.csproj [ADDED] HyperOperatingSystem.Demo/Program1.cs [ADDED] HyperOperatingSystem.Demo/assets/.gitignore [ADDED] HyperOperatingSystem.Demo/assets/qiita-9ea0c8fd43b61b01a8da.json [ADDED] HyperOperatingSystem.Demo/assets/youtube-playlists.json [ADDED] HyperOperatingSystem.Internals/.gitignore [ADDED] HyperOperatingSystem.Internals/build.sh [ADDED] HyperOperatingSystem.Internals/lib.dll.cs [ADDED] HyperOperatingSystem.Test/Add2Test.cs [ADDED] HyperOperatingSystem.Test/HyperOperatingSystem.Test.csproj [ADDED] HyperOperatingSystem.bash [ADDED] HyperOperatingSystem.sln [ADDED] HyperOperatingSystem/EasyZipCrc32.cs [ADDED] HyperOperatingSystem/EasyZipDate.cs [ADDED] HyperOperatingSystem/EasyZipFileEntry.cs [ADDED] HyperOperatingSystem/EasyZipStorer.cs [ADDED] HyperOperatingSystem/HyperOperatingSystem.csproj [ADDED] HyperOperatingSystem/README.md [ADDED] HyperOperatingSystem/Shared/EasyPartialHttpStream.cs [ADDED] HyperOperatingSystem/Shared/EasyTextEmbedder.cs [ADDED] HyperOperatingSystem/Shared/HyperOperatingSystem.cs [ADDED] Targets/Program.cs [ADDED] Targets/Targets.csproj [ADDED] _release.sh [ADDED] _sed.sh [ADDED] build [ADDED] ide.rider [ADDED] ide.vs [ADDED] run-10.sh [ADDED] run-462.sh [ADDED] version.txt

---
+++ HyperOperatingSystem.Test/Add2Test.cs
@@ -0,0 +1,31 @@
+// ReSharper disable once CheckNamespace
+namespace Global.HyperOperatingSystem.Test;
+using Global;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using static Global.EasyObject;
+public class Add2Test {
+    [SetUp]
+    public void Setup() {
+        Echo($"⭕️✅❝𝑪𝒉𝒆𝒄𝒌：{FullName(this)}#Setup() called❞✅");
+    }
+    [Test]
+    public void Test01() {
+        AssertIdentical(expected: 333, actual: HyperOperatingSystem.Add2(111, 222));
+        Log("⭕️✅❝𝑪𝒉𝒆𝒄𝒌：▶passed-01❞✅");
+        AssertEquivalent(expected: new object[] { 11, 22, 33 }, actual: NewArray(11, 22, 33));
+        Log("⭕️✅❝𝑪𝒉𝒆𝒄𝒌：▶passed-02❞✅");
+        var data1 = new Dictionary<string, object?> {
+            { "Apple", 1 },
+            { "Banana", 2 },
+            { "List", NewArray(11, 22, 33) },
+        };
+        var data2 = new { Apple = 1, Banana = 2, List = NewArray(11, 22, 33) };
+        Echo(data1, "⭕️✅❝𝑪𝒉𝒆𝒄𝒌：▶data1❞✅");
+        Echo(data2, "⭕️✅❝𝑪𝒉𝒆𝒄𝒌：▶data2❞✅");
+        AssertEquivalent(expected: data1, actual: data2);
+        Log("⭕️✅❝𝑪𝒉𝒆𝒄𝒌：▶passed-03❞✅");
+    }
+}
---
+++ HyperOperatingSystem/README.md
@@ -0,0 +1,3 @@
+# HyperOperatingSystem
+
+[![NuGet version (HyperOperatingSystem)](https://img.shields.io/nuget/v/HyperOperatingSystem.svg?style=flat-square)](https://www.nuget.org/packages/HyperOperatingSystem/)
